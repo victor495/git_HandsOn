641592828ec1d9f7d091862b26e9b307b36c88df
Victor Lorenzana <victor.lorenzana@uvic.cat>
2018-10-09 08:08:43 -0700

    programa 100% acabat

--- seqClass.sh
+++ seqClass.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 seq=$1
 seq=$(echo $seq | tr a-z A-Z)
-if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
-    echo "The sequence is RNA"
-  else
+if [[ $seq =~ ^[ACGTU]+$ ]]; then #entras las sequencia
+ if [[ $seq =~ T ]]; then #si la sequencia conte T llavors:
+  if [[ $seq =~ U ]]; then #si la sequencia que conte T tambe conte U:
+    echo "The sequence is DNA or RNA"
+   else # si conte T pero no U:
+       echo "the sequence is DNA"
+   fi
+ elif [[ $seq =~ U ]] ; then #si no conte T mira si conte U:
+    echo "the sequence is RNA"
+  else #si no conte T ni U:
     echo "The sequence can be DNA or RNA"
   fi
-else
+else #si directament la sequencia conte algun caracter que no sigui A,C,T,U,G:
   echo "The sequence is not DNA nor RNA"
 fi
-#esto es para el push
-#cambios a local level
